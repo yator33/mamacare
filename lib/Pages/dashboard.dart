706b48b8fa660f96531895e66b042396ea8638ab
yator <cherutichfelix33@gmail.com>
2025-02-23 16:36:37 +0300

    commit

--- lib/Pages/dashboard.dart
+++ lib/Pages/dashboard.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class DashBoard extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text(
         "DashBoard",
-      style: TextStyle(fontWeight: FontWeight.w900, fontSize: 28),
+        style: TextStyle(fontWeight: FontWeight.w900, fontSize: 28),
       ),
     );
   }
 }
+
+///////////////////////////////////
